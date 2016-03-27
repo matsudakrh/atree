5f0c349f47428207bdcb3b5e0c4590cccc24ebbd
matsuda <matsuda.ddcm@gmail.com>
2016-03-27 10:26:09 +0900

    config.jsのcoffeescript化

--- gulp/tasks/json.js
+++ gulp/tasks/json.js
@@ -3,5 +3,5 @@ var config = require('../config').path;
 
 gulp.task( 'json', function () {
     gulp.src( config.sourceDir + 'json/**.json' )
-    .pipe( gulp.dest( config.publicDir + 'json' ));
+        .pipe( gulp.dest( config.publicDir + 'json' ));
 });
